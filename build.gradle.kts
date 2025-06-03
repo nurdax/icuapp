27e16a015f74968d89f4b529e9ce577051a22f1a
nurdaulettashkarayev <jayleo@mail.ru>
2025-06-03 23:56:07 +0300

    ktorapp

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ application {
     mainClass = "io.ktor.server.netty.EngineMain"
 }
 
+
 repositories {
     mavenCentral()
 }
@@ -59,12 +60,10 @@ dependencies {
     testImplementation(libs.kotlin.test.junit)
 }
 
-tasks {
-    jar {
-        manifest {
-            attributes["Main-Class"] = "io.ktor.server.netty.EngineMain"
-        }
-        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-        from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "io.ktor.server.netty.EngineMain"
     }
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
 }
